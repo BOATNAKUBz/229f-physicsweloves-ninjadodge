07e04e1784eaffd0d709f050450bf2a0692b45af
BOATNAKUBz <Natthaphon.sueb@bumail.net>
2026-05-02 22:49:45 +0700

    AddKunai

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -70,7 +70,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) moveInput = -1;
         if (Input.GetKey(KeyCode.D)) moveInput = 1;
 
-        if (Input.GetKeyDown(KeyCode.Q) && !isDashing && Time.time >= lastDashTime + dashCooldown)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing && Time.time >= lastDashTime + dashCooldown)
         {
             StartDash();
         }
@@ -148,7 +148,7 @@ public class PlayerController : MonoBehaviour
         Projectile p = proj.GetComponent<Projectile>();
         if (p != null)
         {
-            p.SetDirection(direction);
+            p.SetDirection(direction); // ใช้ต่อได้
         }
     }
 
